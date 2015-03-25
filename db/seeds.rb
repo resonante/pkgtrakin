74d607fc3f2cf5b5fecfedd91247a1930d3c37a9
Javier Sánchez <js@resonante.net>
2015-03-24 22:16:55 -0430

    Test users.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Setting.create(company_name: "DoralXport")
+Setting.create(company_name: "PkgTrackin")
+
 user = User.create(email: "wizard.resonant@gmail.com", password: "cricket79", :language => 'en')
+testadmin = User.create(email: "testadmin@pkgtrackin.local", password: "testadmin01", :language => 'en')
+testuser = User.create(email: "testuser@pkgtrackin.local", password: "testuser01", :language => 'en')
+
 Profile.create(user: user)
-Assignment.create(role: Role.create(name: "Administrator"), user: user)
-Role.create(name: "User")
+Profile.create(user: testuser)
+
+adminrole = Role.create(name: "Administrator")
+userrole = Role.create(name: "User")
+
+Assignment.create(role: adminrole, user: user)
+Assignment.create(role: adminrole, user: testadmin)
+Assignment.create(role: userrole, user: testuser)
+
+
 Sender.create(name: "Amazon", description: "Amazon.com")
